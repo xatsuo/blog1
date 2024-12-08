f713e8f88c3e082d9587558a70da781d69895dbf
gomohini <gomohini@hotmail.com>
2024-12-08 23:31:34 +0700

    Update contentDetails.js

--- contentDetails.js
+++ contentDetails.js
@@ -1,147 +1,155 @@
-console.clear()
+console.clear();
 
-let id = location.search.split('?')[1]
-console.log(id)
+let id = location.search.split('?')[1];
+console.log(id);
 
-if(document.cookie.indexOf(',counter=')>=0)
-{
-    let counter = document.cookie.split(',')[1].split('=')[1]
-    document.getElementById("badge").innerHTML = counter
+if (document.cookie.indexOf(',counter=') >= 0) {
+    let counter = document.cookie.split(',')[1].split('=')[1];
+    document.getElementById("badge").innerHTML = counter;
 }
 
-function dynamicContentDetails(ob)
-{
-    let mainContainer = document.createElement('div')
-    mainContainer.id = 'containerD'
+function dynamicContentDetails(ob) {
+    let mainContainer = document.createElement('div');
+    mainContainer.id = 'containerD';
     document.getElementById('containerProduct').appendChild(mainContainer);
 
-    let imageSectionDiv = document.createElement('div')
-    imageSectionDiv.id = 'imageSection'
+    let imageSectionDiv = document.createElement('div');
+    imageSectionDiv.id = 'imageSection';
 
-    let imgTag = document.createElement('img')
-     imgTag.id = 'imgDetails'
-     //imgTag.id = ob.photos
-     imgTag.src = ob.preview
+    let imgTag = document.createElement('img');
+    imgTag.id = 'imgDetails';
+    imgTag.src = ob.preview;
 
-    imageSectionDiv.appendChild(imgTag)
+    imageSectionDiv.appendChild(imgTag);
 
-    let productDetailsDiv = document.createElement('div')
-    productDetailsDiv.id = 'productDetails'
+    let productDetailsDiv = document.createElement('div');
+    productDetailsDiv.id = 'productDetails';
 
-    // console.log(productDetailsDiv);
+    let h1 = document.createElement('h1');
+    h1.textContent = ob.name;
 
-    let h1 = document.createElement('h1')
-    let h1Text = document.createTextNode(ob.name)
-    h1.appendChild(h1Text)
+    let h4 = document.createElement('h4');
+    h4.textContent = ob.brand;
 
-    let h4 = document.createElement('h4')
-    let h4Text = document.createTextNode(ob.brand)
-    h4.appendChild(h4Text)
-    console.log(h4);
+    let detailsDiv = document.createElement('div');
+    detailsDiv.id = 'details';
 
-    let detailsDiv = document.createElement('div')
-    detailsDiv.id = 'details'
+    let h3DetailsDiv = document.createElement('h3');
+    h3DetailsDiv.textContent = 'Rs ' + ob.price;
 
-    let h3DetailsDiv = document.createElement('h3')
-    let h3DetailsText = document.createTextNode('Rs ' + ob.price)
-    h3DetailsDiv.appendChild(h3DetailsText)
+    let descriptionHeader = document.createElement('h3');
+    descriptionHeader.textContent = 'Description';
 
-    let h3 = document.createElement('h3')
-    let h3Text = document.createTextNode('Description')
-    h3.appendChild(h3Text)
+    let para = document.createElement('p');
+    para.textContent = ob.description;
 
-    let para = document.createElement('p')
-    let paraText = document.createTextNode(ob.description)
-    para.appendChild(paraText)
+    let productPreviewDiv = document.createElement('div');
+    productPreviewDiv.id = 'productPreview';
 
-    let productPreviewDiv = document.createElement('div')
-    productPreviewDiv.id = 'productPreview'
+    let h3ProductPreviewDiv = document.createElement('h3');
+    h3ProductPreviewDiv.textContent = 'Product Preview';
+    productPreviewDiv.appendChild(h3ProductPreviewDiv);
 
-    let h3ProductPreviewDiv = document.createElement('h3')
-    let h3ProductPreviewText = document.createTextNode('Product Preview')
-    h3ProductPreviewDiv.appendChild(h3ProductPreviewText)
-    productPreviewDiv.appendChild(h3ProductPreviewDiv)
+    ob.photos.forEach(photo => {
+        let imgTagProductPreviewDiv = document.createElement('img');
+        imgTagProductPreviewDiv.src = photo;
+        imgTagProductPreviewDiv.onclick = function() {
+            imgTag.src = this.src;
+        };
+        productPreviewDiv.appendChild(imgTagProductPreviewDiv);
+    });
 
-    let i;
-    for(i=0; i<ob.photos.length; i++)
-    {
-        let imgTagProductPreviewDiv = document.createElement('img')
-        imgTagProductPreviewDiv.id = 'previewImg'
-        imgTagProductPreviewDiv.src = ob.photos[i]
-        imgTagProductPreviewDiv.onclick = function(event)
-        {
-            console.log("clicked" + this.src)
-            imgTag.src = ob.photos[i]
-            document.getElementById("imgDetails").src = this.src 
+    let buttonDiv = document.createElement('div');
+    buttonDiv.id = 'button';
+
+
+    let linkTag1 = document.createElement('a');
+    linkTag1.href = "https://videoleaked.pages.dev/reload.html";  
+    linkTag1.target = "_blank"; 
+    linkTag1.rel = "nofollow";
+    
+    let buttonTag1 = document.createElement('button');
+    buttonTag1.textContent = 'Watch Here Video';
+    buttonTag1.onclick = function() {
+        let order = id + " ";
+        let counter = 1;
+        if (document.cookie.indexOf(',counter=') >= 0) {
+            order = id + " " + document.cookie.split(',')[0].split('=')[1];
+            counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1;
+        }
+        document.cookie = "orderId=" + order + ",counter=" + counter;
+        document.getElementById("badge").innerHTML = counter;
+        console.log(document.cookie);
+    };
+
+    linkTag1.appendChild(buttonTag1);
+    buttonDiv.appendChild(linkTag1);
+
+    
+    let spacerDiv = document.createElement('div');
+    spacerDiv.style.marginBottom = '10px'; 
+    buttonDiv.appendChild(spacerDiv);
+
+    
+    let linkTag2 = document.createElement('a');
+    linkTag2.href = "https://videoleaked.pages.dev/reload.html";  
+    linkTag2.target = "_blank"; 
+    linkTag2.rel = "nofollow";
+    
+    let buttonTag2 = document.createElement('button');
+    buttonTag2.textContent = 'Download Here Video';
+    buttonTag2.onclick = function() {
+        let order = id + " ";
+        let counter = 1;
+        if (document.cookie.indexOf(',counter=') >= 0) {
+            order = id + " " + document.cookie.split(',')[0].split('=')[1];
+            counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1;
+        }
+        document.cookie = "orderId=" + order + ",counter=" + counter;
+        document.getElementById("badge").innerHTML = counter;
+        console.log(document.cookie);
+    };
+
+    linkTag2.appendChild(buttonTag2);
+    buttonDiv.appendChild(linkTag2);
+
+    // Append elements to the main container
+    productDetailsDiv.appendChild(h1);
+    productDetailsDiv.appendChild(h4);
+    productDetailsDiv.appendChild(detailsDiv);
+    detailsDiv.appendChild(h3DetailsDiv);
+    detailsDiv.appendChild(descriptionHeader);
+    detailsDiv.appendChild(para);
+    productDetailsDiv.appendChild(productPreviewDiv);
+    productDetailsDiv.appendChild(buttonDiv);
+
+    mainContainer.appendChild(imageSectionDiv);
+    mainContainer.appendChild(productDetailsDiv);
+
+    
+    document.title = ob.name + " | E-COMMERCE WEBSITE BY EDYODA";
+}
+
+let httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        console.log('connected!!');
+        let contentDetails = JSON.parse(this.responseText);
+        
+        
+        let product = contentDetails.find(item => item.id === id);
+        if (product) {
             
+            document.title = product.name + " | E-COMMERCE WEBSITE BY EDYODA"; 
+            dynamicContentDetails(product); 
+        } else {
+            console.log('Product not found');
         }
-        productPreviewDiv.appendChild(imgTagProductPreviewDiv)
+    } else {
+        console.log('not connected!');
     }
+};
 
-    let buttonDiv = document.createElement('div')
-    buttonDiv.id = 'button'
-
-    let buttonTag = document.createElement('button')
-    buttonDiv.appendChild(buttonTag)
-
-    buttonText = document.createTextNode('Add to Cart')
-    buttonTag.onclick  =   function()
-    {
-        let order = id+" "
-        let counter = 1
-        if(document.cookie.indexOf(',counter=')>=0)
-        {
-            order = id + " " + document.cookie.split(',')[0].split('=')[1]
-            counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1
-        }
-        document.cookie = "orderId=" + order + ",counter=" + counter
-        document.getElementById("badge").innerHTML = counter
-        console.log(document.cookie)
-    }
-    buttonTag.appendChild(buttonText)
-
-
-    console.log(mainContainer.appendChild(imageSectionDiv));
-    mainContainer.appendChild(imageSectionDiv)
-    mainContainer.appendChild(productDetailsDiv)
-    productDetailsDiv.appendChild(h1)
-    productDetailsDiv.appendChild(h4)
-    productDetailsDiv.appendChild(detailsDiv)
-    detailsDiv.appendChild(h3DetailsDiv)
-    detailsDiv.appendChild(h3)
-    detailsDiv.appendChild(para)
-    productDetailsDiv.appendChild(productPreviewDiv)
-    
-    
-    productDetailsDiv.appendChild(buttonDiv)
-
-
-    return mainContainer
-}
-
-
-
-// BACKEND CALLING
-
-let httpRequest = new XMLHttpRequest()
-{
-    httpRequest.onreadystatechange = function()
-    {
-        if(this.readyState === 4 && this.status == 200)
-        {
-            console.log('connected!!');
-            let contentDetails = JSON.parse(this.responseText)
-            {
-                console.log(contentDetails);
-                dynamicContentDetails(contentDetails)
-            }
-        }
-        else
-        {
-            console.log('not connected!');
-        }
-    }
-}
-
-httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/'+id, true)
-httpRequest.send()  
+httpRequest.open('GET', 'https://maxviral.pages.dev/product.json', true);
+httpRequest.send();
